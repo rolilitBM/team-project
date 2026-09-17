1368279eb238f637d9b691336cfc101216f74508
Mark Malisew <markmalischew@gmail.com>
2026-09-17 21:14:58 +0700

    Добавил файл реализации malischew.cpp

---
+++ malischew.cpp
@@ -0,0 +1,9 @@
+#include "malischew.h"
+#include <cmath>
+double triangleArea(double a, double h) {
+    return a * h / 2;
+}
+
+double trianglePerimeter(double a, double b, double c) {
+    return a+b+c;
+}

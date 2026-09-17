6e93550e47e7b5144da057ba309358774ee1b139
Mark Malisew <markmalischew@gmail.com>
2026-09-17 22:17:43 +0700

    feat: перенесены функции варианта 4, добавлены пункты меню 3-4

--- main.cpp
+++ main.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
+#include "malischew.h"
 using namespace std;
 
 int main()
 {
+double a, b, c, h;
+
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
     int choice;
 
     do
     {
-        cout << "0. Выход.";
+        cout << "3. Площадь\n";
+	cout << "4. Периметр\n";
+        cout << "0. Выход.\n";
         cout << "Выберите пункт: ";
         cin >> choice;
         switch (choice)
         {
         // Место вставки: меню калькулятор геомтерической прогрессии
-        // Место вставки: меню калькулятор треугольника
+        case 3:
+                do {
+                cout << "Введите сторону a и высоту h (в сантиметрах): ";
+                cin >> a >> h;
+		cout << "\n=== Площадь треугольника со стороной a и высотой h ===\n";
+                } while(a <= 0 || h <=0);
+                cout << "Площадь = " << triangleArea(a, h) << " См\n";
+                break;
+	case 4:
+                do{
+		cout << "Введите стороны a,b,c (в сантиметрах): ";
+		cin >> a >> b >> c;
+		cout << "\n=== Периметр треугольника со сторонами a,b,c ===\n";
+                } while(a <= 0 || b <= 0 || c <= 0);
+		cout << "Периметр = " << trianglePerimeter(a,b,c) << " См\n";
+		break;
         case 0:
             cout << "Работа завершена.\n";
             break;
---
+++ malischew.cpp
@@ -0,0 +1,11 @@
+#include "malischew.h"
+#include <cmath>
+// Площадь треугольника со стороной a и высотой h
+double triangleArea(double a, double h) {
+    return a * h / 2;
+}
+
+// Периметр треугольника со сторонами a,b,c
+double trianglePerimeter(double a, double b, double c) {
+    return a+b+c;
+}

c5d9ff71592b1f047ee5eb365c1a7575e37f23d1
Mark Malisew <markmalischew@gmail.com>
2026-09-17 21:14:21 +0700

    Добавил заголовочный файл malischew.h

---
+++ malischew.h
@@ -0,0 +1,6 @@
+#ifndef MALISCHEW_H
+#define MALISCHEW_H
+// Функции варианта 4 (Малышев М.А.)
+double triangleArea(double a, double h);
+double trianglePerimeter(double a, double b, double c);
+#endif // MALISCHEW_H
